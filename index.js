3bf30d7158e168b688d019c6c70f74f5c6457672
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2020-12-08 20:19:21 +0100

    fix: add required dependencies, remove unnecessary rules and reorder rule set

--- index.js
+++ index.js
@@ -9,44 +9,37 @@ module.exports = {
 	  "jsx": true,
 	  "useJSXTextNode": true
 	},
-	"extends": ["airbnb","plugin:@typescript-eslint/recommended"],
-	"plugins": ["@typescript-eslint/eslint-plugin"],
+	"extends": [
+		"airbnb",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"plugins": [
+		"@typescript-eslint"
+	],
 	"rules": {
-		"import/no-extraneous-dependencies": 0,
-		"@typescript-eslint/explicit-function-return-type": 0,
-		"react/jsx-filename-extension": 0,
-		"import/prefer-default-export": 0,
-		"react/jsx-indent": 0,
-		"react/prop-types": 0,
-		"semi": [2, "never"],
-		"quotes": [2, "double"],
-		"react/button-has-type": 0, //https://github.com/yannickcr/eslint-plugin-react/issues/1555
-		"@typescript-eslint/explicit-member-accessibility": 0,
+		"semi": [2, "always"],
+		"quotes": [2, "single"],
 		"indent": 0,
-		"@typescript-eslint/indent": [2, 4],
-		"react/jsx-indent-props": [2, 4],
 		"lines-between-class-members": 0,
 		"import/no-unresolved": 0,
-		"@typescript-eslint/member-delimiter-style": [
-			2, {
-				"multiline": {
-					"delimiter": "none"
-				},
-				"singleline": {
-					"delimiter": "comma",
-					"requireLast": true
-				}
-			}
-		],
-		"jsx-a11y/label-has-associated-control": 0, // Does not work correctly with component inputs
-		"jsx-a11y/label-has-for": 0,
-		"spaced-comment": 0,
-		"react/jsx-one-expression-per-line": [1, { allow: "single-child" } ],
-		"camelcase": "off",
-		"@typescript-eslint/camelcase": 0,
-		"react-hooks/exhaustive-deps": 0,
+		"no-alert": 1,
 		"max-len": [1, {code: 150}],
-		"arrow-parens": 0,
 		"import/extensions": 0,
+		"max-classes-per-file": 0,
+		"no-use-before-define": 0,
+
+		"@typescript-eslint/indent": [2, 2],
+		"@typescript-eslint/no-use-before-define": 0,
+		"@typescript-eslint/no-unused-vars": 0,
+		"@typescript-eslint/triple-slash-reference": 0,
+		"@typescript-eslint/explicit-function-return-type": 0,
+
+		"import/no-extraneous-dependencies": 0,
+		"import/prefer-default-export": 0,
+
+		"react/jsx-filename-extension": 0,
+		"react/prop-types": 0,
+		"react/jsx-indent": [2, 2],
+		"react/jsx-indent-props": [2, 2]
 	}
 }
